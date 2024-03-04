497e57cee830abe84c276e4ae27b8f97f86a14a8
kmpk <kmpk.gm@gmail.com>
2024-03-04 15:37:35 +0400

    Add DB init schema script

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,29 @@
+create schema if not exists "bank";
+
+create table if not exists bank."user"
+(
+    id         serial primary key,
+    birth_date date         not null,
+    email      varchar(255)
+        constraint user_email_key unique,
+    full_name  varchar(255) not null,
+    login      varchar(255) not null
+        constraint user_login_key unique,
+    password   varchar(255) not null,
+    phone      varchar(255)
+        constraint user_phone_key unique
+);
+
+create table if not exists bank.account
+(
+    id              integer not null
+        primary key
+        constraint account_id_user_id_fkey
+            references bank."user",
+    balance         numeric(38, 2)
+        constraint account_balance_check check (balance >= 0),
+    initial_balance numeric(38, 2)
+);
+
+
+
